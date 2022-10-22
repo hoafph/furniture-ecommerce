34902012b2925fc312bb704cab5ec281b0cc1cda
Hoa Pham <hoapeo2208@gmail.com>
2022-10-22 12:35:02 +0700

    prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "remixicon/fonts/remixicon.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'remixicon/fonts/remixicon.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import App from "./App";
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
